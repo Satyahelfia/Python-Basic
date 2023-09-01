2e6c2fbda59531d65cba853ce9fdff772b1268e5
Satya Helfi <108466903+Satyahelfia@users.noreply.github.com>
2023-09-01 14:10:49 +0700

    Add files via upload

---
+++ course 0.py
@@ -0,0 +1,18 @@
+#variabel adalah tempat penyimpanan data
+
+#menaruh / assignment nilai
+a = 10
+x = 5
+panjang = 1000
+
+print ("Nilai a = ", a)
+print ("Nilai x = ", x)
+print ("Nilai panjang = ", panjang)
+
+#penamaan variabel tidak boleh memakai spasi
+nilai_y = 15
+nilai y = 15 #contoh penamaan yang salah
+
+#penamaan variabel tidak boleh menggunakan awalan angka
+satya10 = 10
+10satya = 10 #contoh penamaan yang salah
